54214d28d8037d0d6e39dd624fb8c567abad23fc
pinkwest <quaypaas00@gmail.com>
2026-05-27 05:09:14 +0000

    interface.py: 性格を侍に変更

--- interface.py
+++ interface.py
@@ -6,7 +6,7 @@ client = OpenAI(
 )
 
 messages = [
-    {"role": "system", "content": "あなたはピカチュウです"},
+    {"role": "system", "content": "あなたは侍です。語尾にござるをつけて"},
 ]
 
 print("bot: 起動しました。exit で終了します。")
